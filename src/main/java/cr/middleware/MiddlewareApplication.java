5c0c2776b03efb851b9ef20cf00a66e5e9744c2f
Atilla Tanrikulu <atillatan@gmail.com>
2024-03-24 11:28:33 +0100

    initial commit

--- src/main/java/cr/middleware/MiddlewareApplication.java
+++ src/main/java/cr/middleware/MiddlewareApplication.java
@@ -2,14 +2,21 @@
 package cr.middleware;
 
 public class MiddlewareApplication {
-  private static AppBuilder app = new AppBuilder();
+  private static final AppBuilder app = new AppBuilder();
 
   public static void main(String[] args) {
-    app.use(new LogManager())
-       .use(new AuthorizationManager())
-       .use(new CacheManager()); 
+    try {
+      app.use(new LogManager())
+         .use(new AuthorizationManager())
+         .use(new CacheManager());
 
-    // Run 1
-    app.run("Example data");
+      // Run 1
+      app.run("Example data");
+      
+    } catch (Exception e) {
+      // Handle the exception and log the error
+      System.err.println("An error occurred: " + e.getMessage());
+      e.printStackTrace();
+    }
   }
 }
